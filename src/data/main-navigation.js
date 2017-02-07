93d72149b0ec693f938ce4c13bc513b1f0f00160
Italo <italo@runroom.com>
2017-02-07 18:33:34 +0100

    Update samples, Add main navigation data

---
+++ src/data/main-navigation.js
@@ -0,0 +1,18 @@
+export default {
+    title: 'First',
+    subsections: [
+        {
+            title: 'Secondary 1',
+            anchors: [
+                {
+                    title: 'Anchor 1',
+                    anchor: '#Anchor1'
+                },
+                {
+                    title: 'Anchor 2',
+                    anchor: '#Anchor2'
+                }
+            ]
+        }
+    ]
+}
